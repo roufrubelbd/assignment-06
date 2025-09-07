f7a1895d709883947869b9b014713741a352f0c7
roufrubelbd <roufrubelbd@gmail.com>
2025-09-07 12:28:03 +0100

    trees car added

---
+++ index.js
@@ -0,0 +1,147 @@
+// -------------- get all document by id -----------------
+
+const categoriesContainer = document.getElementById('categories_container');
+const allCategoriesContainerById = document.getElementById('all_categories_container_by_id');
+const treesCardContainer = document.getElementById('trees_card_container');
+// console.log(allCategoriesContainerById.childNodes)
+
+
+// ------------------ 
+// all plants
+// -------------------
+
+
+// ---------------- load categories menu from api -----------------
+const loadCategories = async () => {
+    try {
+        const response = await fetch('https://openapi.programming-hero.com/api/categories');
+        const data = await response.json();
+        const categories = data.categories;
+        showCategories(categories);
+    } catch (error) {
+        console.log('Error loading categories:', error);
+    }
+};
+
+
+// ---------------- show categories menu in ui -----------------
+const showCategories = (categories) => {
+    categoriesContainer.innerHTML = "";
+    categories.forEach(category => {
+       categoriesContainer.innerHTML += `
+        <li id="${category.id}" class="category-item hover:bg-green-600 hover:text-white px-3 py-2 rounded-md font-medium text-sm cursor-pointer">
+            ${category.category_name}
+        </li>
+       `;
+    });
+
+    //  allCategoriesContainerById
+    allCategoriesContainerById.addEventListener("click", (e) => {
+    const allLi = allCategoriesContainerById.querySelectorAll("li");
+    // console.log(allLi[0].innerHTML);
+    allLi.forEach((li) => {
+      li.classList.remove("bg-green-600", "text-white");
+    });
+
+    if (e.target.localName === "li") {
+    //   showLoading()
+      e.target.classList.add("bg-green-600", "text-white");
+       loadTreesByCategory(e.target.id);
+    }
+  });
+};
+
+
+// ------------- load trees by category id -----------------
+const loadTreesByCategory = async (categoryId) => {
+    if (categoryId === "all_trees_01") {
+        loadAllPlants();
+        // return;
+    } else {
+        try {
+        const response = await fetch(`https://openapi.programming-hero.com/api/category/${categoryId}`);
+        const data = await response.json();
+        const trees = data.plants;
+        showTreesByCategory(trees);
+    } catch (error) {
+        console.log('Error loading trees by category:', error);
+    }
+};
+};
+
+
+// ------------- show trees by category id -----------------
+const showTreesByCategory = (trees) => {
+    treesCardContainer.innerHTML = "";
+    trees.forEach(tree => {
+        console.log(tree)
+        treesCardContainer.innerHTML += `
+        <div class="card bg-white shadow-sm h-fit">
+  <figure class="h-40">
+    <img
+      src="${tree.image}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${tree.name}</h2>
+    <p class="text-sm line-shorten">${tree.description}</p>
+    <div class="card-actions justify-between mt-4 font-semibold">
+        <span class="text-sm bg-green-200 px-2 py-1 rounded-md">${tree.category}</span>
+        <span class="text-sm">$ ${tree.price}</span>
+    </div>
+    <div class="card-actions justify-center">
+      <button class="btn bg-green-600 text-white rounded-4xl w-full">Add to Cart</button>
+    </div>
+  </div>
+</div>
+       `;
+    });
+};
+
+
+
+// ---------------- load all plants from api -----------------
+const loadAllPlants = async () => {
+    try {
+        const response = await fetch('https://openapi.programming-hero.com/api/plants');
+        const data = await response.json();
+        const allPlants = data.plants;
+        showAllPlants(allPlants);
+    } catch (error) {
+        console.log('Error loading all plants:', error);
+    }
+};
+
+
+// ---------------- show all plants in ui -----------------
+const showAllPlants = (allPlants) => {
+     treesCardContainer.innerHTML = "";
+    allPlants.forEach(plant => {
+        // console.log(plant)
+        treesCardContainer.innerHTML += `
+        <div class="card bg-white shadow-sm">
+  <figure class="h-40">
+    <img
+      src="${plant.image}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${plant.name}</h2>
+    <p class="text-sm line-shorten">${plant.description}</p>
+    <div class="card-actions justify-between mt-4 font-semibold">
+        <span class="text-sm bg-green-200 px-2 py-1 rounded-md">${plant.category}</span>
+        <span class="text-sm">$ ${plant.price}</span>
+    </div>
+    <div class="card-actions justify-center">
+      <button class="btn bg-green-600 text-white rounded-4xl w-full">Add to Cart</button>
+    </div>
+  </div>
+</div>
+       `;
+    });
+};
+
+
+// -------------- call the load functions -----------------
+loadAllPlants();
+loadCategories();
